69424450507b52973a7da966295cce2d2c0dbec0
S_8 <flowerarranging10@gmail.com>
2018-06-25 01:02:18 +0900

    third commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user
+  
   def index
     @posts = Post.all.order(created_at: :desc)
   end
@@ -8,33 +10,39 @@ class PostsController < ApplicationController
   end
   
   def new
+    @post = Post.new
   end
   
   def create
     @post = Post.new(content: params[:content])
     if @post.save
-	  redirect_to("/posts/index")
-	else
-	  render("posts/new")
+      flash[:notice] = "投稿を作成しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
     end
   end
+  
   def edit
-  	@post = Post.find_by(id: params[:id])
+    @post = Post.find_by(id: params[:id])
   end
-
+  
   def update
-  	@post = Post.find_by(id: params[:id])
-  	@post.content =params[:content]
-  	if @post.save
-  		redirect_to("/posts/index")
-  	else
-	  	render("posts/edit")
-  	end
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
+    end
   end
-
+  
   def destroy
-  	@post = Post.find_by(id: params[:id])
+    @post = Post.find_by(id: params[:id])
     @post.destroy
-  	redirect_to("/posts/index")
+    flash[:notice] = "投稿を削除しました"
+    redirect_to("/posts/index")
   end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_21_170854) do
+ActiveRecord::Schema.define(version: 2018_06_23_174053) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2018_06_21_170854) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
+    t.string "password"
   end
 
 end
